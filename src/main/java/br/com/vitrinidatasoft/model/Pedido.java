0eb245445c050a21b9b18f3a2ebc5fa49c06b22e
marioolinto <marioolinto@gmail.com>
2016-03-25 00:04:48 -0400

    commit das classes pedido, pedidoitem funcionando alteração do método hashcode das classes

---
+++ src/main/java/br/com/vitrinidatasoft/model/Pedido.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.vitrinidatasoft.model;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author mrhell
+ */
+@Entity
+public class Pedido implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;    
+    @ManyToOne
+    private Cliente cliente;
+    private String numero;
+    @Temporal (TemporalType.TIMESTAMP)
+    private Calendar dataPedido;
+    @Temporal (TemporalType.TIMESTAMP)
+    private Calendar dataEntrega;
+    private String endereco;
+    private String observacao;
+    private Float desconto;
+    private Float valor;
+    @OneToMany(mappedBy = "pedido", cascade=CascadeType.ALL, orphanRemoval = true)
+    private Set<PedidoItem> itens;   
+    
+    public Pedido(){
+        this.itens = new HashSet<>();
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+       
+        public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Calendar getDataPedido() {
+        return dataPedido;
+    }
+
+    public void setDataPedido(Calendar dataPedido) {
+        this.dataPedido = dataPedido;
+    }
+
+    public Calendar getDataEntrega() {
+        return dataEntrega;
+    }
+
+    public void setDataEntrega(Calendar dataEntrega) {
+        this.dataEntrega = dataEntrega;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public Float getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(Float desconto) {
+        this.desconto = desconto;
+    }
+
+    public Float getValor() {
+        return valor;
+    }
+
+    public void setValor(Float valor) {
+        this.valor = valor;
+    }
+    
+     public Set<PedidoItem> getItens() {
+        return itens;
+    }
+
+    public void setItens(Set<PedidoItem> itens) {
+        this.itens = itens;
+    }
+    
+    public void addItem(PedidoItem item) {            
+        item.setPedido(this);
+        this.itens.add(item);
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Pedido)) {
+            return false;
+        }
+        Pedido other = (Pedido) object;
+        if ((this.id == null && other.id != null) || 
+           (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.com.vitrinidatasoft.model.Pedido[ id=" + id + " ]";
+    }
+}
---
+++ src/main/java/br/com/vitrinidatasoft/model/PedidoItem.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.vitrinidatasoft.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author mrhell
+ */
+@Entity
+public class PedidoItem implements Serializable {    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Pedido pedido;
+    @ManyToOne
+    private Produto produto;
+    private Integer quantidade;        
+    private Float valor;    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Float getValor() {
+        return valor;
+    }
+
+    public void setValor(Float valor) {
+        this.valor = valor;
+    }            
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : getProduto().hashCode());
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof PedidoItem)) {
+            return false;
+        }
+        PedidoItem other = (PedidoItem) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.com.vitrinidatasoft.model.PedidoItem[ id=" + id + " ]";
+    }
+    
+}
